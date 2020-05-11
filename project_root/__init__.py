feac838d40196aea69b97c0a868e25ddb59856c4
yontilevin <yontilevin@gmail.com>
2020-05-11 17:54:29 +0300

    folder level fix

--- project_root/__init__.py
+++ project_root/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import configparser
+from pathlib import Path
 config = configparser.ConfigParser()
 
 
@@ -11,10 +12,10 @@ if os.path.exists(project_root_file):
     if config['Project_Root']['use_absolute_path']:
         project_root = config['Project_Root']['absolute_path']
     else:
-        project_root = os.path.abspath(__name__)
+        project_root = Path(os.path.abspath(__name__))
         for i in range(int(config['Project_Root']['folder_level'])):
             project_root = project_root.parent
- 
+ 	project_root = str(project_root)
 if project_root is not None and project_root not in sys.path:  
     package_absolute_path = str(os.path.abspath(__name__))
     project_root = project_root.rstrip("\'").lstrip("\'")
